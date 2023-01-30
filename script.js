877b851e6021973504729d690dba8e2c5fae359c
Adhami3310 <khaleel.aladhami@gmail.com>
2023-01-29 22:46:55 -0500

    init donut design

---
+++ script.js
@@ -0,0 +1,45 @@
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+const SPRINKLE_HEIGHT = 16;
+const SPRINKLE_WIDTH = 5;
+const COLORS = ["#FEA9A7", "#94CAEF", "#F57CAA", "#FFB600", "#FE5E5B"];
+const DENSITY = 0.0015;
+
+
+
+
+document.querySelectorAll(".sprinkles").forEach(cont => {
+    const count = Math.floor(DENSITY*cont.clientHeight*cont.clientWidth);
+    const xValues = shuffle([...Array(count).keys()].map(i => i/count*cont.clientWidth));
+    let i = 0;
+
+    function addPoint() {
+        if (i >= count) return;
+        i += 1;
+        const node = document.createElement("div");
+        node.classList.add("sprinkle");
+        node.style.height = `${SPRINKLE_HEIGHT}px`;
+        node.style.width = `${SPRINKLE_WIDTH}px`;
+        node.style.transform = `translateX(${xValues[i]}px) translateY(${Math.random() * cont.clientHeight - SPRINKLE_HEIGHT / 2}px) rotate(${Math.random()*360}deg)`;
+        node.style.background = COLORS[Math.floor(Math.random() * COLORS.length)];
+        cont.appendChild(node);
+        setTimeout(addPoint, 10);
+    }
+
+    addPoint();
+});
