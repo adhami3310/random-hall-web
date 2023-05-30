9d2b5af5816ac862e205d31dcf6855ba4ad2e73e
Adhami3310 <khaleel.aladhami@gmail.com>
2023-05-30 11:41:13 -0400

    update all to use pug

--- _site/script.js
+++ _site/script.js
@@ -1,17 +1,10 @@
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
+    let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
-
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-
     return array;
 }
 
@@ -25,8 +18,8 @@ const CHUNK = 1;
 
 
 document.querySelectorAll(".sprinkles").forEach(cont => {
-    const count = Math.floor(DENSITY*cont.clientHeight*cont.clientWidth);
-    const xValues = shuffle([...Array(count).keys()].map(i => i/count*cont.clientWidth));
+    const count = Math.floor(DENSITY * cont.clientHeight * cont.clientWidth);
+    const xValues = shuffle([...Array(count).keys()].map(i => i / count * cont.clientWidth));
     let i = 0;
 
     function addPoint() {
@@ -36,11 +29,11 @@ document.querySelectorAll(".sprinkles").forEach(cont => {
             node.classList.add("sprinkle");
             node.style.height = `${SPRINKLE_HEIGHT}px`;
             node.style.width = `${SPRINKLE_WIDTH}px`;
-            node.style.transform = `translateX(${xValues[i]}px) translateY(${Math.random() * (cont.clientHeight - 0.8 * 3 * SPRINKLE_HEIGHT) + 0.8 * SPRINKLE_HEIGHT}px) rotate(${Math.random()*360}deg)`;
+            node.style.transform = `translateX(${xValues[i]}px) translateY(${Math.random() * (cont.clientHeight - 0.8 * 3 * SPRINKLE_HEIGHT) + 0.8 * SPRINKLE_HEIGHT}px) rotate(${Math.random() * 360}deg)`;
             node.style.background = COLORS[Math.floor(Math.random() * COLORS.length)];
-            cont.appendChild(node);            
+            cont.appendChild(node);
         }
-        setTimeout(addPoint, 1000/(count / CHUNK));
+        setTimeout(addPoint, 1000 / (count / CHUNK));
     }
 
     addPoint();
