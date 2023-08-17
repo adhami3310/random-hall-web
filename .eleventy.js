cb24031e9100785f5323773c1e89328ed3307d7d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2023-08-17 18:55:22 -0400

    update paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("script.js");
     eleventyConfig.addPassthroughCopy("cookbook.pdf");
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 };
